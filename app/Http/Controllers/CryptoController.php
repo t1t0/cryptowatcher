2001b89653b959c3eabc3c95d697e54483a2ff3b
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2024-11-16 21:36:10 +0100

    CryptoApiRepositoryInterface injected in CryptoController

--- app/Http/Controllers/CryptoController.php
+++ app/Http/Controllers/CryptoController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\CryptoApiRepositoryInterface;
 use Illuminate\Http\Request;
 
 class CryptoController extends Controller
 {
-    //
+    protected $cryptoRepository; 
+    
+    public function __construct(CryptoApiRepositoryInterface $cryptoRepository) { 
+        $this->cryptoRepository = $cryptoRepository; 
+    } 
 }

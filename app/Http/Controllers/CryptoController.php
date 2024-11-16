465f33aa8e200a31a290ec80516ef093ced3fd3f
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2024-11-16 22:30:03 +0100

    Controller for Crypto defined

--- app/Http/Controllers/CryptoController.php
+++ app/Http/Controllers/CryptoController.php
@@ -12,4 +12,14 @@ class CryptoController extends Controller
     public function __construct(CryptoApiRepositoryInterface $cryptoRepository) { 
         $this->cryptoRepository = $cryptoRepository; 
     } 
+
+    public function index(string $sort, string $currencyType) { 
+        $cryptos = $this->cryptoRepository->listLatestCoins($sort, $currencyType); 
+        return view('cryptos.index', compact('cryptos')); 
+    } 
+    
+    public function show(string $symbol, string $currency) { 
+        $coin = $this->cryptoRepository->getLatestCoinQuote($symbol, $currency);
+         return view('cryptos.show', compact('coin')); 
+    }
 }

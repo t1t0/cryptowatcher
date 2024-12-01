79d14eb0b400b7d264354ab6d84c152a812282c2
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2024-12-01 21:27:52 +0100

    Component update to add New coins to the list and show in the list only those coins that are not already be added to the coin list

--- app/Livewire/CryptoList.php
+++ app/Livewire/CryptoList.php
@@ -3,7 +3,9 @@
 namespace App\Livewire;
 
 use App\CryptoApiRepositoryInterface;
+use App\Models\Crypto;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
 use Livewire\Component;
 
 class CryptoList extends Component
@@ -11,21 +13,23 @@ class CryptoList extends Component
     protected $cryptoRepository;
     public $cryptos = [];
     public $coins = [];
+    public $newCoin;
 
-    public function mount(CryptoApiRepositoryInterface $cryptoRepository)
+    public function render(CryptoApiRepositoryInterface $cryptoRepository)
     {
         $this->cryptoRepository = $cryptoRepository;
         $responsedcoins = $this->cryptoRepository->listLatestCoins('market_cap', 'coins');
-        $this->coins = collect($responsedcoins)->map(function ($coin) {
-            return ['label' => $coin['name'] . ' (' . $coin['symbol'] . ')', 'value' => $coin['symbol']];
-        });
-    }
+        $userCoins = Auth::user()->cryptos->pluck('symbol')->toArray();
 
-    public function render()
-    {
+        // Filter out coins that are already in the user's list
+        $this->coins = collect($responsedcoins)->filter(function ($coin) use ($userCoins) {
+            return !in_array($coin['symbol'], $userCoins);
+        })->map(function ($coin) {
+            return ['label' => $coin['name'], 'value' => $coin['symbol']];
+        });
+
         $this->cryptos = [];
-        $coins = Auth::user()->cryptos;
-        foreach ($coins as $coin) {
+        foreach (Auth::user()->cryptos as $coin) {
             $response = $this->cryptoRepository->getLatestCoinQuote($coin->symbol, 'EUR');
             $this->cryptos[] = [
                 'name' => $response[$coin->symbol]['name'],
@@ -34,5 +38,16 @@ class CryptoList extends Component
             ];
         }
         return view('livewire.crypto-list');
+    }
+
+
+    public function addCoinToList()
+    {
+        $coin = collect($this->coins)->firstWhere('value', $this->newCoin);
+        $coinToAdd = new Crypto();
+        $coinToAdd->symbol = $coin['value'];
+        $coinToAdd->name = $coin['label'];
+        $coinToAdd->user_id = Auth::user()->id;
+        $coinToAdd->save();
     }
 }
--- resources/views/livewire/crypto-list.blade.php
+++ resources/views/livewire/crypto-list.blade.php
@@ -1,7 +1,12 @@
 <div>
     <div class='bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg my-6'>
-        <div class="p-6 text-gray-900 dark:text-gray-100">
-            <x-ts-select.native :options="$coins" select="label:label|value:value" />
+        <div class="p-6 text-gray-900 dark:text-gray-100 flex">
+            <div class='grow m-4'>
+                <x-ts-select.native :options="$coins" select="label:label|value:value" wire:model="newCoin" />
+            </div>
+            <div class='m-4'>
+                <x-ts-button round wire:click="addCoinToList">Add Coin</x-ts-button>
+            </div>
         </div>
     </div>
 
